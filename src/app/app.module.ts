0f8c487aa2dfb37c1be3c442cd11697de9e888c4
HAPPIESTMINDS\Sudarshan.selvam <sudarshan.selvam@happiestminds.com>
2017-06-15 11:10:36 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
